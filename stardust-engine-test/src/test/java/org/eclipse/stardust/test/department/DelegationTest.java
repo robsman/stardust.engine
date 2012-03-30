bdad14f5d46a2269bf63372b6703585695aa1a84
Nicolas Werlein <nicolas.werlein@sungard.com>
2012-03-30 12:15:40 +0000

    added SVN keyword 'Revision"
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@55140 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/department/DelegationTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/department/DelegationTest.java
@@ -45,7 +45,7 @@ import org.junit.rules.TestRule;
  * </p>
  * 
  * @author Nicolas.Werlein
- * @version $Revision: $
+ * @version $Revision$
  */
 public class DelegationTest extends LocalJcrH2Test
 {

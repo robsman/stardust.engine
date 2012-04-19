0227177160fdbb1470fddce2329617d84899c0b6
Nicolas Werlein <nicolas.werlein@sungard.com>
2012-04-19 09:28:48 +0000

    added svn:keyword 'Revision'
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@55616 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/data/PrimitiveDataInOutDataPathsTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/data/PrimitiveDataInOutDataPathsTest.java
@@ -38,7 +38,7 @@ import org.junit.rules.TestRule;
  * </p>
  * 
  * @author Nicolas.Werlein
- * @version $Revision: $
+ * @version $Revision$
  */
 public class PrimitiveDataInOutDataPathsTest extends LocalJcrH2Test
 {

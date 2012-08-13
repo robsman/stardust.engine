6f9d13817cb024111c842dfd32ec0661e4ced8b0
Nicolas Werlein <nicolas.werlein@sungard.com>
2012-08-13 09:36:10 +0000

    CRNT-25638: set SPI to status 'stable'
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@58362 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/spi/cluster/ClusterSafeObjectProvider.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/spi/cluster/ClusterSafeObjectProvider.java
@@ -10,10 +10,8 @@ import org.eclipse.stardust.common.annotations.UseRestriction;
 /**
  * @author Nicolas.Werlein
  * @version $Revision: $
- * 
- * TODO (nw) change status as soon as it's stable
  */
-@SPI(status = Status.Experimental, useRestriction = UseRestriction.Internal)
+@SPI(status = Status.Stable, useRestriction = UseRestriction.Internal)
 public interface ClusterSafeObjectProvider
 {
    public <K, V> Map<K, V> clusterSafeMap(final String mapId);

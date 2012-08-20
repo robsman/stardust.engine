9d0a0a898233df8425bd546822e6c5dcae390afe
Nicolas Werlein <nicolas.werlein@sungard.com>
2012-08-20 09:26:08 +0000

    CRNT-25638: set svn:keywords
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@58525 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-spring/src/main/java/org/eclipse/stardust/engine/spring/integration/jca/SpringAppContextHazelcastJcaConnectionFactoryProvider.java
+++ stardust-engine-spring/src/main/java/org/eclipse/stardust/engine/spring/integration/jca/SpringAppContextHazelcastJcaConnectionFactoryProvider.java
@@ -20,7 +20,7 @@ import org.springframework.context.ApplicationContext;
 
 /**
  * @author Nicolas.Werlein
- * @version $Revision: $
+ * @version $Revision$
  */
 public class SpringAppContextHazelcastJcaConnectionFactoryProvider implements HazelcastJcaConnectionFactoryProvider
 {

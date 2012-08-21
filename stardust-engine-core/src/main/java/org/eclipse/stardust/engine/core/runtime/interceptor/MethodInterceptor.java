d95f451369a56fa443df78b0dd71486135b46213
Barry Grotjahn <barry.grotjahn@sungard.com>
2012-08-21 08:46:30 +0000

    Jira-ID: CRNT-25266 adapted license
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@58565 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/runtime/interceptor/MethodInterceptor.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/runtime/interceptor/MethodInterceptor.java
@@ -1,14 +1,22 @@
-/*******************************************************************************
- * Copyright (c) 2011 SunGard CSA LLC and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Contributors:
- *    SunGard CSA LLC - initial API and implementation and/or initial documentation
- *******************************************************************************/
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.eclipse.stardust.engine.core.runtime.interceptor;
+
+//Copied from org.aopalliance.intercept.
 
 import java.io.Serializable;
 

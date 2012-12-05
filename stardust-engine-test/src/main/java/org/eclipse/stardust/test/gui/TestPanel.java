49445567d061694b1de0b314d3b6d3105e542814
Holger Prause <holger.prause@sungard.com>
2012-12-05 13:27:34 +0000

    Jira-ID: CRNT-27091 CRNT-27091: moved test class to stardust-engine-test project
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@61373 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ stardust-engine-test/src/main/java/org/eclipse/stardust/test/gui/TestPanel.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.stardust.test.gui;
+
+import java.awt.Container;
+
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+
+import org.eclipse.stardust.engine.core.compatibility.gui.utils.DateEntry;
+import org.eclipse.stardust.engine.core.compatibility.gui.utils.DateTimeEntry;
+import org.eclipse.stardust.engine.core.compatibility.gui.utils.TimeEntry;
+import org.eclipse.stardust.engine.core.compatibility.gui.utils.spinner.JSpinnerTime;
+
+
+/**
+ * @author rsauer
+ * @version $Revision$
+ */
+public class TestPanel extends JDialog
+{
+   public TestPanel()
+   {
+      Container content = getContentPane();
+
+      content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+      content.add(new DateEntry());
+      content.add(new TimeEntry());
+      content.add(new DateTimeEntry());
+      content.add(new JSpinnerTime());
+
+      pack();
+   }
+
+   public static void main(String[] args)
+   {
+      TestPanel panel = new TestPanel();
+      panel.show();
+   }
+}

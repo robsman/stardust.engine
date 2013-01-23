020ed6e77e8832773ebd4447e3455805116a75b2
Thomas Wolfram <thomas.wolfram@sungard.com>
2013-01-23 14:47:49 +0000

    License Header updated.
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/engine@62316 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/runtime/beans/QueryServiceImpl.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/runtime/beans/QueryServiceImpl.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2011 SunGard CSA LLC and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    SunGard CSA LLC - initial API and implementation and/or initial documentation
- *******************************************************************************/
+* Copyright (c) 2011, 2013 SunGard CSA LLC and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* SunGard CSA LLC - initial API and implementation and/or initial documentation
+*******************************************************************************/
 package org.eclipse.stardust.engine.core.runtime.beans;
 
 import java.io.ByteArrayOutputStream;

061f75e5e8a00c83432545aa253e3613a73329fc
Thomas Wolfram <thomas.wolfram@sungard.com>
2013-04-29 08:58:05 +0000

    Jira-ID: CRNT-28911 Modified to derive base Xpath from Data Path defined in Application to consider Data Mappings correctly!
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_7_1_x/stardust/engine@64563 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Thomas Wolfram <thomas.wolfram@sungard.com>

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/extensions/transformation/runtime/serialization/MessageSerializationApplicationInstance.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/extensions/transformation/runtime/serialization/MessageSerializationApplicationInstance.java
@@ -61,6 +61,8 @@ public class MessageSerializationApplicationInstance implements
 
    private org.w3c.dom.DOMImplementation domImpl = null;
    private IXPathMap xPathMap;
+   
+   private String inDataPath;
 
    private Document schemaDocument;
 
@@ -82,6 +84,9 @@ public class MessageSerializationApplicationInstance implements
          throw new RuntimeException("Could not find IN data mapping");
       }
       DataMapping dataMapping = (DataMapping) allInDataMappings.iterator().next();
+      
+      inDataPath = dataMapping.getDataPath();
+      
       IData data = model.findData(dataMapping.getDataId());		
       xPathMap = getXPathMap(data);
       structuredDataConverter = newStructuredDataConverter(xPathMap);
@@ -242,7 +247,7 @@ public class MessageSerializationApplicationInstance implements
             trace.info("Setting access point " + name + " to value " + inputMessage + ".");
          }
 
-         Node[] nodes = structuredDataConverter.toDom(inputMessage, "", true);
+         Node[] nodes = structuredDataConverter.toDom(inputMessage, this.inDataPath, true);
 
          if (nodes == null || nodes.length == 0)
          {

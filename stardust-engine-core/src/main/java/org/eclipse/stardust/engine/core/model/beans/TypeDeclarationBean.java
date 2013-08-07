f424c8144139d06d6ea049bc24d8fb157ff50fe6
Rainer Pielmann <rainer.pielmann@sungard.com>
2013-08-07 07:04:49 +0000

    Jira-ID: CRNT-29563 Web Base Modeler: Deleting SDT used as Parent type in another SDT does not updated the xpdl in turn wsdl not getting loaded in Soap UI.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@66529 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Rainer Pielmann <rainer.pielmann@sungard.com>

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/model/beans/TypeDeclarationBean.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/model/beans/TypeDeclarationBean.java
@@ -10,15 +10,32 @@
  *******************************************************************************/
 package org.eclipse.stardust.engine.core.model.beans;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+
 import org.eclipse.stardust.common.StringUtils;
-import org.eclipse.stardust.engine.api.model.*;
+import org.eclipse.stardust.engine.api.model.IExternalPackage;
+import org.eclipse.stardust.engine.api.model.IModel;
+import org.eclipse.stardust.engine.api.model.ITypeDeclaration;
+import org.eclipse.stardust.engine.api.model.IXpdlType;
+import org.eclipse.stardust.engine.api.model.Inconsistency;
+import org.eclipse.stardust.engine.api.model.PredefinedConstants;
 import org.eclipse.stardust.engine.core.model.utils.IdentifiableElementBean;
+import org.eclipse.stardust.engine.core.model.utils.ModelElementList;
 import org.eclipse.stardust.engine.core.preferences.configurationvariables.ConfigurationVariableUtils;
+import org.eclipse.stardust.engine.core.struct.StructuredDataConstants;
+import org.eclipse.xsd.XSDComplexTypeDefinition;
+import org.eclipse.xsd.XSDImport;
 import org.eclipse.xsd.XSDSchema;
+import org.eclipse.xsd.XSDSchemaContent;
+import org.eclipse.xsd.XSDTypeDefinition;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -32,7 +49,7 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
 {
 
    private final IXpdlType xpdlType;
-   
+
    TypeDeclarationBean()
    {
       this.xpdlType = null;
@@ -49,7 +66,7 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
       {
          setAttribute(PredefinedConstants.BROWSABLE_ATT, Boolean.TRUE);
       }
-      
+
       this.xpdlType = xpdlType;
    }
 
@@ -69,7 +86,7 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
    public void checkConsistency(List inconsistencies)
    {
       checkId(inconsistencies);
-      
+
       // check for unique Id
       ITypeDeclaration td = ((IModel) getModel()).findTypeDeclaration(getId());
       if (td != null && td != this)
@@ -77,7 +94,9 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
          inconsistencies.add(new Inconsistency("Duplicate ID for type declaration '" +
                getName() + "'.", this, Inconsistency.ERROR));
       }
-      
+
+      this.validateParentReferences((IModel) getModel(), inconsistencies, td);
+
       // check for usage of variables
       if (xpdlType instanceof SchemaTypeBean)
       {
@@ -87,7 +106,7 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
          checkForVariables(inconsistencies, element);
       }
    }
-   
+
    private void checkForVariables(List inconsistencies, Node node)
    {
       if (node instanceof Element)
@@ -106,7 +125,7 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
             {
                attribute = element.getAttribute("name");
             }
-            
+
             if (attribute != null)
             {
                Matcher variablesMatcher = ConfigurationVariableUtils
@@ -129,4 +148,125 @@ public class TypeDeclarationBean extends IdentifiableElementBean implements ITyp
          }
       }
    }
+
+   private IModel getRefModel(IModel model, QName qname)
+   {
+      if (XMLConstants.NULL_NS_URI != qname.getNamespaceURI())
+      {
+         List<IExternalPackage> packs = model.getExternalPackages();
+         if (packs != null)
+         {
+            for (Iterator<IExternalPackage> i = packs.iterator(); i.hasNext();)
+            {
+               IExternalPackage refPack = i.next();
+               String modelHRef = refPack.getHref();
+               if (modelHRef != null)
+               {
+                  if (modelHRef.equals(refPack.getHref()))
+                  {
+                     return refPack.getReferencedModel();
+                  }
+               }
+            }
+         }
+      }
+      return model;
+   }
+
+   private void validateParentReferences(IModel model, List inconsistencies,
+         ITypeDeclaration declaration)
+   {
+      if (xpdlType instanceof SchemaTypeBean)
+      {
+         SchemaTypeBean schemaType = (SchemaTypeBean) xpdlType;
+         XSDSchema xsdSchema = schemaType.getSchema();
+         for (Iterator<XSDTypeDefinition> i = xsdSchema.getTypeDefinitions().iterator(); i.hasNext();)
+         {
+            XSDTypeDefinition typeDefinition = i.next();
+            if (typeDefinition instanceof XSDComplexTypeDefinition)
+            {
+               XSDComplexTypeDefinition complexType = (XSDComplexTypeDefinition) typeDefinition;
+               XSDTypeDefinition baseType = complexType.getBaseType();
+               if (baseType != null)
+               {
+                  String baseTypeNameSpace = baseType.getTargetNamespace();
+                  XSDImport baseTypeImport = this.getImportByNamespace(
+                        schemaType.getSchema(), baseTypeNameSpace);
+                  if (baseTypeImport != null)
+                  {
+                     String location = ((XSDImport) baseTypeImport).getSchemaLocation();
+                     String typeId = location.substring(StructuredDataConstants.URN_INTERNAL_PREFIX.length());
+                     QName qname = QName.valueOf(typeId);
+                     model = getRefModel(model, qname);
+                     ModelElementList<TypeDeclarationBean> referedDeclarations = model.getTypeDeclarations();
+                     if (getTypeDeclaration(referedDeclarations, qname.getLocalPart()) == null)
+                     {
+                        String message = MessageFormat.format(
+                              "TypeDeclaration ''{0}'': referenced parent type ''{1}'' not found.", //$NON-NLS-1$
+                              declaration.getId(), typeId);
+                        inconsistencies.add(new Inconsistency(message, this,
+                              Inconsistency.ERROR));
+                     }
+                  }
+
+               }
+            }
+         }
+      }
+   }
+
+   private TypeDeclarationBean getTypeDeclaration(
+         ModelElementList<TypeDeclarationBean> referedDeclarations, String localPart)
+   {
+      for (Iterator<TypeDeclarationBean> i = referedDeclarations.iterator(); i.hasNext();)
+      {
+         TypeDeclarationBean typeDeclaration = i.next();
+         if (typeDeclaration.getId().equals(localPart))
+         {
+            return typeDeclaration;
+         }
+      }
+      return null;
+   }
+
+   private XSDImport getImportByNamespace(XSDSchema schema, String nameSpace)
+   {
+      List<XSDImport> xsdImports = getImports(schema);
+      if (xsdImports != null)
+      {
+         for (Iterator<XSDImport> i = xsdImports.iterator(); i.hasNext();)
+         {
+            XSDImport xsdImport = i.next();
+            String importNameSpace = xsdImport.getNamespace();
+            if (nameSpace.equals(importNameSpace))
+            {
+               return xsdImport;
+            }
+         }
+      }
+      return null;
+   }
+
+   public static List<XSDImport> getImports(XSDSchema schema)
+   {
+      List<XSDImport> xsdImports = new ArrayList<XSDImport>();
+      List<XSDSchemaContent> contents = schema.getContents();
+      Iterator<XSDSchemaContent> it = contents.iterator();
+      while (it.hasNext())
+      {
+         XSDSchemaContent content = (XSDSchemaContent) it.next();
+         if (content instanceof XSDImport)
+         {
+            xsdImports.add((XSDImport) content);
+         }
+      }
+      if (!xsdImports.isEmpty())
+      {
+         return xsdImports;
+      }
+      return null;
+   }
+
+
+
 }

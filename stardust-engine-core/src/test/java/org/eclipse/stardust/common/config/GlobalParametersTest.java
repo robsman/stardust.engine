da2e9a7efce7d8d121783dac6d5898f14105a3c4
Nicolas Werlein <nicolas.werlein@sungard.com>
2013-08-12 09:32:30 +0000

    CRNT-30059: extended test case
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@66626 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Nicolas Werlein <nicolas.werlein@sungard.com>

--- stardust-engine-core/src/test/java/org/eclipse/stardust/common/config/GlobalParametersTest.java
+++ stardust-engine-core/src/test/java/org/eclipse/stardust/common/config/GlobalParametersTest.java
@@ -33,43 +33,65 @@ import org.junit.Test;
  */
 public class GlobalParametersTest
 {
+   private static final int THREAD_COUNT = 1000;
+   
    private static final String THREAD_ID_VALUE = "<a value>";
    
    @Ignore("CRNT-30059")
    @Test
    public void testConcurrentModificationViaSet() throws InterruptedException
    {
-      final int nThreads = 100;
-      final ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
-      for (int i=0; i<nThreads; i++)
+      executeTest(new ThreadSubmission()
       {
-         executorService.submit(new GlobalParametersModifierViaSet(String.valueOf(i)));
-      }
-      executorService.shutdown();
-      final boolean terminatedGracefully = executorService.awaitTermination(1, TimeUnit.MINUTES);
-      if ( !terminatedGracefully)
+         @Override
+         public void submitThreads(final ExecutorService executorService)
+         {
+            for (int i=0; i<THREAD_COUNT; i++)
+            {
+               executorService.submit(new GlobalParametersModifierViaSet(String.valueOf(i)));
+            }
+         }
+      });
+   }
+   
+   @Test
+   public void testConcurrentModificationViaGetOrInitialize() throws InterruptedException
+   {
+      executeTest(new ThreadSubmission()
       {
-         Assert.fail("Timeout while waiting for executor service termination.");
-      }
-      
-      final GlobalParameters globals = GlobalParameters.globals();
-      for (int i=0; i<nThreads; i++)
-      {
-         assertThat("Thread ID: " + i, (String) globals.get(String.valueOf(i)), equalTo(THREAD_ID_VALUE));
-      }
+         @Override
+         public void submitThreads(final ExecutorService executorService)
+         {
+            for (int i=0; i<THREAD_COUNT; i++)
+            {
+               executorService.submit(new GlobalParametersModifierViaGetOrInitialize(String.valueOf(i)));
+            }
+         }
+      });      
    }
    
    @Ignore("CRNT-30059")
    @Test
    public void testConcurrentModificationViaSetAndGetOrInitialize() throws InterruptedException
    {
-      final int nThreads = 100;
-      final ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
-      for (int i=0; i<nThreads; i=i+2)
+      executeTest(new ThreadSubmission()
       {
-         executorService.submit(new GlobalParametersModifierViaSet(String.valueOf(i)));
-         executorService.submit(new GlobalParametersModifierViaGetOrInitialize(String.valueOf(i+1)));
-      }
+         @Override
+         public void submitThreads(final ExecutorService executorService)
+         {
+            for (int i=0; i<THREAD_COUNT; i=i+2)
+            {
+               executorService.submit(new GlobalParametersModifierViaSet(String.valueOf(i)));
+               executorService.submit(new GlobalParametersModifierViaGetOrInitialize(String.valueOf(i+1)));
+            }
+         }
+      });
+   }
+   
+   private void executeTest(final ThreadSubmission threadSubmission) throws InterruptedException
+   {
+      final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+      threadSubmission.submitThreads(executorService);
       executorService.shutdown();
       final boolean terminatedGracefully = executorService.awaitTermination(1, TimeUnit.MINUTES);
       if ( !terminatedGracefully)
@@ -78,10 +100,15 @@ public class GlobalParametersTest
       }
       
       final GlobalParameters globals = GlobalParameters.globals();
-      for (int i=0; i<nThreads; i++)
+      for (int i=0; i<THREAD_COUNT; i++)
       {
          assertThat("Thread ID: " + i, (String) globals.get(String.valueOf(i)), equalTo(THREAD_ID_VALUE));
       }
+   }
+   
+   private static interface ThreadSubmission
+   {
+      public void submitThreads(final ExecutorService executorService);
    }
    
    private static final class GlobalParametersModifierViaSet implements Callable<Void>

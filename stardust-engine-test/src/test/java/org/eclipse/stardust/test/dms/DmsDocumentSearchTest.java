a9e347119b64914124ef843cb0873782429adff0
Nicolas Werlein <nicolas.werlein@sungard.com>
2013-08-16 07:09:11 +0000

    removed unused import
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@66732 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Nicolas Werlein <nicolas.werlein@sungard.com>

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/dms/DmsDocumentSearchTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/dms/DmsDocumentSearchTest.java
@@ -37,7 +37,6 @@ import org.eclipse.stardust.test.api.setup.TestServiceFactory;
 import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;

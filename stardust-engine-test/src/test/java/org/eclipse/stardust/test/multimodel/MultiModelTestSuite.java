7124c943e16f56b274b5b3cd4f193366d38b63d0
Robert Sauer <robert.sauer@sungard.com>
2014-01-09 17:00:20 +0000

    Adding multi-model test suite.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@69892 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/multimodel/MultiModelTestSuite.java
@@ -0,0 +1,43 @@
+/**********************************************************************************
+ * Copyright (c) 2012 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ **********************************************************************************/
+package org.eclipse.stardust.test.multimodel;
+
+import static org.eclipse.stardust.test.util.TestConstants.MOTU;
+import static org.eclipse.stardust.test.multimodel.MultiModelConstants.PROVIDER_MODEL_NAME;
+import static org.eclipse.stardust.test.multimodel.MultiModelConstants.CONSUMER_MODEL_NAME;
+
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSuiteSetup;
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSetup.ForkingServiceMode;
+import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * <p>
+ * This test suite bundles tests that verify multiple, cross-referencing models.
+ * </p>
+ *
+ * @author Robert Sauer
+ * @version $Revision: 56996 $
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+               RetrieveModelDetailsTest.class
+             })
+public class MultiModelTestSuite
+{
+   /* test suite */
+
+   @ClassRule
+   public static final LocalJcrH2TestSuiteSetup testSuiteSetup = new LocalJcrH2TestSuiteSetup(new UsernamePasswordPair(MOTU, MOTU), ForkingServiceMode.NATIVE_THREADING, PROVIDER_MODEL_NAME, CONSUMER_MODEL_NAME);
+}

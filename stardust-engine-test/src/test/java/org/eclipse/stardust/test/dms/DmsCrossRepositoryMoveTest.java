ef62ee15aaa277ea487df8ad74f9beee0c96454a
Roland Stamm <roland.stamm@sungard.com>
2014-05-08 07:32:53 +0000

    Jira-ID: CRNT-32765 Added tests for same repository move and cross repository move with target folderId.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@72467 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/dms/DmsCrossRepositoryMoveTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/dms/DmsCrossRepositoryMoveTest.java
@@ -17,6 +17,7 @@ import static org.eclipse.stardust.test.util.TestConstants.MOTU;
 import org.eclipse.stardust.engine.api.runtime.DmsUtils;
 import org.eclipse.stardust.engine.api.runtime.Document;
 import org.eclipse.stardust.engine.api.runtime.DocumentManagementService;
+import org.eclipse.stardust.engine.api.runtime.Folder;
 import org.eclipse.stardust.engine.core.spi.dms.IRepositoryConfiguration;
 import org.eclipse.stardust.engine.core.spi.dms.RepositoryIdUtils;
 import org.eclipse.stardust.test.api.setup.DmsAwareTestMethodSetup;
@@ -76,11 +77,12 @@ public class DmsCrossRepositoryMoveTest
    @After
    public void cleanup() throws InterruptedException
    {
+      getDms().removeFolder(RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID), true);
       getDms().unbindRepository(TEST_REPO_ID);
    }
 
    @Test
-   public void testCrossRepositoryMove() throws InterruptedException
+   public void testCrossRepositoryMoveWithTargetPath() throws InterruptedException
    {
       Document doc = getDms().getDocument(RepositoryIdUtils.addRepositoryId("/test.txt", TEST_REPO_ID));
 
@@ -105,6 +107,108 @@ public class DmsCrossRepositoryMoveTest
             RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID));
 
       Assert.assertNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), null)));
+   }
+
+   @Test
+   public void testCrossRepositoryMoveWithTargetId() throws InterruptedException
+   {
+      Document doc = getDms().getDocument(RepositoryIdUtils.addRepositoryId("/test.txt", TEST_REPO_ID));
+
+      Assert.assertNotNull(getDms().getDocument(doc.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), null)));
+
+      // move from testRepo to default
+      Folder targetFolder = getDms().getFolder(RepositoryIdUtils.addRepositoryId("/", SYSTEM_REPO_ID));
+      Document movedDoc = getDms().moveDocument(doc.getId(), targetFolder.getId());
+
+      Assert.assertNull(getDms().getDocument(doc.getId()));
+      Assert.assertEquals(SYSTEM_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), TEST_REPO_ID)));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), null)));
+
+      // move from default to testRepo
+      Folder targetFolder2 = getDms().getFolder(RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID));
+      Document movedDoc2 = getDms().moveDocument(movedDoc.getId(), targetFolder2.getId());
+
+      Assert.assertNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), null)));
+   }
+
+
+   @Test
+   public void testSameRepositoryMoveWithTargetPath() throws InterruptedException
+   {
+      Document doc = getDms().getDocument(RepositoryIdUtils.addRepositoryId("/test.txt", TEST_REPO_ID));
+      getDms().createFolder(RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID), DmsUtils.createFolderInfo("test"));
+
+      Assert.assertNotNull(getDms().getDocument(doc.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), null)));
+
+      // move from testRepo to testRepo
+      Document movedDoc = getDms().moveDocument(doc.getId(),
+            RepositoryIdUtils.addRepositoryId("/test", TEST_REPO_ID));
+
+      Assert.assertNotNull(getDms().getDocument(doc.getId()));
+      Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), null)));
+
+      // move from testRepo to testRepo
+      Document movedDoc2 = getDms().moveDocument(movedDoc.getId(),
+            RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID));
+
+      Assert.assertNotNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc2.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), null)));
+   }
+
+   @Test
+   public void testSameRepositoryMoveWithTargetId() throws InterruptedException
+   {
+      Document doc = getDms().getDocument(RepositoryIdUtils.addRepositoryId("/test.txt", TEST_REPO_ID));
+      getDms().createFolder(RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID), DmsUtils.createFolderInfo("test"));
+
+      Assert.assertNotNull(getDms().getDocument(doc.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(doc.getId(), null)));
+
+      // move from testRepo to testRepo
+      Folder targetFolder = getDms().getFolder(RepositoryIdUtils.addRepositoryId("/test", TEST_REPO_ID));
+      Document movedDoc = getDms().moveDocument(doc.getId(), targetFolder.getId());
+
+      Assert.assertNotNull(getDms().getDocument(doc.getId()));
+      Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc.getId()));
+      Assert.assertNotNull(getDms().getDocument(movedDoc.getId()));
+      Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), TEST_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), SYSTEM_REPO_ID)));
+      Assert.assertNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc.getId(), null)));
+
+      // move from testRepo to testRepo
+      Folder targetFolder2 = getDms().getFolder(RepositoryIdUtils.addRepositoryId("/", TEST_REPO_ID));
+      Document movedDoc2 = getDms().moveDocument(movedDoc.getId(), targetFolder2.getId());
+
+      Assert.assertNotNull(getDms().getDocument(movedDoc.getId()));
       Assert.assertEquals(TEST_REPO_ID, RepositoryIdUtils.extractRepositoryId(movedDoc2.getId()));
       Assert.assertNotNull(getDms().getDocument(movedDoc2.getId()));
       Assert.assertNotNull(getDms().getDocument(RepositoryIdUtils.replaceRepositoryId(movedDoc2.getId(), TEST_REPO_ID)));

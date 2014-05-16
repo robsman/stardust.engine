0892a7a8c12b9dc363a6032d57789a09b0f146bb
Roland Stamm <roland.stamm@sungard.com>
2014-05-16 08:32:43 +0000

    Jira-ID: CRNT-32913 Added JavaDoc.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@72717 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Roland Stamm <roland.stamm@sungard.com>

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/query/RepositoryPolicy.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/query/RepositoryPolicy.java
@@ -13,27 +13,50 @@ package org.eclipse.stardust.engine.api.query;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * By adding this policy multiple repositories can be target of a query.
+ * The results from the different repositories are merged together.
+ *
+ * @author roland.stamm
+ *
+ */
 public class RepositoryPolicy implements EvaluationPolicy
 {
    private static final long serialVersionUID = -4922399462085360884L;
-   
+
    private List<String> repositoryIds;
 
+   public RepositoryPolicy()
+   {
+      repositoryIds = Collections.EMPTY_LIST;
+   }
+
+   /**
+    * This creates a RepositoryPolicy that includes all available repositories into the search.
+    *
+    * @return The configured RepositoryPolicy.
+    */
    public static RepositoryPolicy includeAllRepositories()
    {
       return new RepositoryPolicy(Collections.EMPTY_LIST);
    }
-   
+
+   /**
+    * This creates a RepositoryPolicy that includes all selected repositories into the search.
+    *
+    * @param repositoryIds The repositorId of all repositories that should be included.
+    * @return The configured RepositoryPolicy.
+    */
    public static RepositoryPolicy includeRepositories(List<String> repositoryIds)
    {
       return new RepositoryPolicy(repositoryIds);
    }
-   
+
    public RepositoryPolicy(List<String> repositoryIds)
    {
       this.repositoryIds = repositoryIds;
    }
-   
+
    public List<String> getRepositoryIds()
    {
       return repositoryIds;

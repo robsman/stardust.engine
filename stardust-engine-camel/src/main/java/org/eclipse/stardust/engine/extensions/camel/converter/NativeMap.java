540f402901927cfb2a2c49678689d07cad772cd3
Fradj Zayen <fradj.zayen@sungard.com>
2014-05-21 09:55:37 +0000

    removed some commented code
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@72813 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-camel/src/main/java/org/eclipse/stardust/engine/extensions/camel/converter/NativeMap.java
+++ stardust-engine-camel/src/main/java/org/eclipse/stardust/engine/extensions/camel/converter/NativeMap.java
@@ -1,209 +1,154 @@
 package org.eclipse.stardust.engine.extensions.camel.converter;
+
 import java.util.Iterator;
 import java.util.Map;
 
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.Wrapper;
 
-
-
 public class NativeMap implements Scriptable, Wrapper
 {
-    private static final long serialVersionUID = 3664761893203964569L;
-    
-    private Map<Object, Object> map;
-    private Scriptable parentScope;
-    private Scriptable prototype;
+   private static final long serialVersionUID = 3664761893203964569L;
 
-    
-    /**
-     * Construct
-     * 
-     * @param scope
-     * @param map
-     * @return  native map
-     */
-    public static NativeMap wrap(Scriptable scope, Map<Object, Object> map)
-    {
-        return new NativeMap(scope, map);
-    }
+   private Map<Object, Object> map;
 
-    /**
-     * Construct
-     * 
-     * @param scope
-     * @param map
-     */
-    public NativeMap(Scriptable scope, Map<Object, Object> map)
-    {
-        this.parentScope = scope;
-        this.map = map;
-    }
+   private Scriptable parentScope;
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Wrapper#unwrap()
-     */
-    public Object unwrap()
-    {
-        return map;
-    }
+   private Scriptable prototype;
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#getClassName()
-     */
-    public String getClassName()
-    {
-        return "NativeMap";
-    }
+   /**
+    * Construct
+    * 
+    * @param scope
+    * @param map
+    * @return native map
+    */
+   public static NativeMap wrap(Scriptable scope, Map<Object, Object> map)
+   {
+      return new NativeMap(scope, map);
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#get(java.lang.String, org.mozilla.javascript.Scriptable)
-     */
-    public Object get(String name, Scriptable start)
-    {
-        // get the property from the underlying QName map
-        if ("length".equals(name))
-        {
-            return map.size();
-        }
-        else
-        {
-            return map.get(name);
-        }
-    }
+   /**
+    * Construct
+    * 
+    * @param scope
+    * @param map
+    */
+   public NativeMap(Scriptable scope, Map<Object, Object> map)
+   {
+      this.parentScope = scope;
+      this.map = map;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#get(int, org.mozilla.javascript.Scriptable)
-     */
-    public Object get(int index, Scriptable start)
-    {
-        Object value =  null;
-        int i=0;
-        Iterator itrValues = map.values().iterator();
-        while (i++ <= index && itrValues.hasNext())
-        {
-            value = itrValues.next();
-        }
-        return value;
-    }
+   public Object unwrap()
+   {
+      return map;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#has(java.lang.String, org.mozilla.javascript.Scriptable)
-     */
-    public boolean has(String name, Scriptable start)
-    {
-        // locate the property in the underlying map
-        return map.containsKey(name);
-    }
+   public String getClassName()
+   {
+      return "NativeMap";
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#has(int, org.mozilla.javascript.Scriptable)
-     */
-    public boolean has(int index, Scriptable start)
-    {
-        return (index >= 0 && map.values().size() > index);
-    }
+   public Object get(String name, Scriptable start)
+   {
+      // get the property from the underlying QName map
+      if ("length".equals(name))
+      {
+         return map.size();
+      }
+      else
+      {
+         return map.get(name);
+      }
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#put(java.lang.String, org.mozilla.javascript.Scriptable, java.lang.Object)
-     */
-    @SuppressWarnings("unchecked")
-    public void put(String name, Scriptable start, Object value)
-    {
-        map.put(name, value);
-    }
+   public Object get(int index, Scriptable start)
+   {
+      Object value = null;
+      int i = 0;
+      Iterator itrValues = map.values().iterator();
+      while (i++ <= index && itrValues.hasNext())
+      {
+         value = itrValues.next();
+      }
+      return value;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#put(int, org.mozilla.javascript.Scriptable, java.lang.Object)
-     */
-    public void put(int index, Scriptable start, Object value)
-    {
-        // TODO: implement?
-    }
+   public boolean has(String name, Scriptable start)
+   {
+      // locate the property in the underlying map
+      return map.containsKey(name);
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#delete(java.lang.String)
-     */
-    public void delete(String name)
-    {
-        map.remove(name);
-    }
+   public boolean has(int index, Scriptable start)
+   {
+      return (index >= 0 && map.values().size() > index);
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#delete(int)
-     */
-    public void delete(int index)
-    {
-        int i=0;
-        Iterator itrKeys = map.keySet().iterator();
-        while (i <= index && itrKeys.hasNext())
-        {
-            Object key = itrKeys.next();
-            if (i == index)
-            {
-                map.remove(key);
-                break;
-            }
-        }
-    }
+   public void put(String name, Scriptable start, Object value)
+   {
+      map.put(name, value);
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#getPrototype()
-     */
-    public Scriptable getPrototype()
-    {
-        return this.prototype;
-    }
+   public void put(int index, Scriptable start, Object value)
+   {}
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#setPrototype(org.mozilla.javascript.Scriptable)
-     */
-    public void setPrototype(Scriptable prototype)
-    {
-        this.prototype = prototype;
-    }
+   public void delete(String name)
+   {
+      map.remove(name);
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#getParentScope()
-     */
-    public Scriptable getParentScope()
-    {
-        return this.parentScope;
-    }
+   public void delete(int index)
+   {
+      int i = 0;
+      Iterator itrKeys = map.keySet().iterator();
+      while (i <= index && itrKeys.hasNext())
+      {
+         Object key = itrKeys.next();
+         if (i == index)
+         {
+            map.remove(key);
+            break;
+         }
+      }
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#setParentScope(org.mozilla.javascript.Scriptable)
-     */
-    public void setParentScope(Scriptable parent)
-    {
-        this.parentScope = parent;
-    }
+   public Scriptable getPrototype()
+   {
+      return this.prototype;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#getIds()
-     */
-    public Object[] getIds()
-    {
-        return map.keySet().toArray();
-    }
+   public void setPrototype(Scriptable prototype)
+   {
+      this.prototype = prototype;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#getDefaultValue(java.lang.Class)
-     */
-    public Object getDefaultValue(Class hint)
-    {
-        return null;
-    }
+   public Scriptable getParentScope()
+   {
+      return this.parentScope;
+   }
 
-    /* (non-Javadoc)
-     * @see org.mozilla.javascript.Scriptable#hasInstance(org.mozilla.javascript.Scriptable)
-     */
-    public boolean hasInstance(Scriptable value)
-    {
-        if (!(value instanceof Wrapper))
-            return false;
-        Object instance = ((Wrapper)value).unwrap();
-        return Map.class.isInstance(instance);
-    }
+   public void setParentScope(Scriptable parent)
+   {
+      this.parentScope = parent;
+   }
 
+   public Object[] getIds()
+   {
+      return map.keySet().toArray();
+   }
+
+   public Object getDefaultValue(Class hint)
+   {
+      return null;
+   }
+
+   public boolean hasInstance(Scriptable value)
+   {
+      if (!(value instanceof Wrapper))
+         return false;
+      Object instance = ((Wrapper) value).unwrap();
+      return Map.class.isInstance(instance);
+   }
 }

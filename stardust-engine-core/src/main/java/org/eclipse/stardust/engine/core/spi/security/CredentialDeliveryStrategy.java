b4f8b2917f563a1ede6059c45314b6ce355891b0
Thomas Wolfram <thomas.wolfram@sungard.com>
2014-06-20 10:03:56 +0000

    Jira-ID: CRNT-29750 Added JavaDoc and Annotations
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/engine@73426 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/spi/security/CredentialDeliveryStrategy.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/core/spi/security/CredentialDeliveryStrategy.java
@@ -1,26 +1,37 @@
 package org.eclipse.stardust.engine.core.spi.security;
 
+import org.eclipse.stardust.common.annotations.SPI;
+import org.eclipse.stardust.common.annotations.Status;
+import org.eclipse.stardust.common.annotations.UseRestriction;
 import org.eclipse.stardust.engine.core.runtime.beans.IUser;
 
 /**
+ * The {@link CredentialDeliveryStrategy} SPI provides an interface to implement
+ * a custom strategy for delivering information that is required to reset a user
+ * password to the user itself. </br> </br> To change the default
+ * {@link MailbasedCredentialDeliveryStrategy} to a custom implementation
+ * declared class in the file
+ * <i>/META-INF/org.eclipse.stardust.engine.core.spi.security
+ * .CredentialDeliveryStrategy</i> needs to be changed to the custom implementation
  * 
  * @author Thomas.Wolfram
- *
+ * 
  */
+@SPI(status = Status.Stable, useRestriction = UseRestriction.Public)
 public interface CredentialDeliveryStrategy {
 
 	/**
 	 * 
-	 * @param user
-	 * @param token
+	 * @param user - the {@link IUser} instance for which the password reset token has been generated for
+	 * @param token - the generated reset token
 	 */
 	void deliverPasswordResetToken(IUser user, String token);
-	
+
 	/**
 	 * 
-	 * @param user
-	 * @param password
+	 * @param user - the {@link IUser} instance for which the password has been reseted
+	 * @param password - the newly generated password for the user
 	 */
 	void deliverNewPassword(IUser user, String password);
-	
+
 }

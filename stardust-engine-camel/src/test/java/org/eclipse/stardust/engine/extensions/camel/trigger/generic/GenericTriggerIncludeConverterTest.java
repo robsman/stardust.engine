53a31a5a3735ca0674633a0c2831556ab12dcd6f
Fradj Zayen <fradj.zayen@sungard.com>
2014-09-30 12:00:00 +0000

    apply date formatter for dates; to avoid test failure in build server.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_1_x/stardust/engine@74943 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-camel/src/test/java/org/eclipse/stardust/engine/extensions/camel/trigger/generic/GenericTriggerIncludeConverterTest.java
+++ stardust-engine-camel/src/test/java/org/eclipse/stardust/engine/extensions/camel/trigger/generic/GenericTriggerIncludeConverterTest.java
@@ -4,48 +4,52 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 import org.eclipse.stardust.common.log.LogManager;
 import org.eclipse.stardust.common.log.Logger;
-import org.eclipse.stardust.engine.api.query.DeployedModelQuery;
 import org.eclipse.stardust.engine.api.query.ProcessInstanceQuery;
 import org.eclipse.stardust.engine.api.query.ProcessInstances;
 import org.eclipse.stardust.engine.api.runtime.AdministrationService;
-import org.eclipse.stardust.engine.api.runtime.Models;
 import org.eclipse.stardust.engine.api.runtime.ProcessInstance;
 import org.eclipse.stardust.engine.api.runtime.ServiceFactory;
 import org.eclipse.stardust.engine.extensions.camel.util.client.ServiceFactoryAccess;
-import org.eclipse.stardust.engine.extensions.camel.util.test.SpringTestUtils;
 
 public class GenericTriggerIncludeConverterTest
 {
-   private static final Logger trace = LogManager.getLogger(GenericTriggerIncludeConverterTest.class.getName());
+   private static final Logger trace = LogManager
+         .getLogger(GenericTriggerIncludeConverterTest.class.getName());
+
    private static ClassPathXmlApplicationContext ctx;
+
    AdministrationService administrationService;
+
    private static ServiceFactoryAccess serviceFactoryAccess;
-   
+
+   public static String formatDate(Date input)
+   {
+      return new SimpleDateFormat("dd-MM-yy").format(input);
+   }
+
    @BeforeClass
-   public static void beforeClass() {
-      ctx = new ClassPathXmlApplicationContext(new String[] {
-            "org/eclipse/stardust/engine/extensions/camel/common/SharedTestContext.xml",
-            "classpath:carnot-spring-context.xml", "classpath:jackrabbit-jcr-context.xml",
-            "classpath:default-camel-context.xml"});
-      serviceFactoryAccess = (ServiceFactoryAccess) ctx.getBean("ippServiceFactoryAccess");
-
-//      File dir = new File("./target/FileDirectory");
-//      dir.mkdirs();
-
+   public static void beforeClass()
+   {
+      ctx = new ClassPathXmlApplicationContext(
+            new String[] {
+                  "org/eclipse/stardust/engine/extensions/camel/common/SharedTestContext.xml",
+                  "classpath:carnot-spring-context.xml",
+                  "classpath:jackrabbit-jcr-context.xml",
+                  "classpath:default-camel-context.xml"});
+      serviceFactoryAccess = (ServiceFactoryAccess) ctx
+            .getBean("ippServiceFactoryAccess");
    }
 
    @Test
@@ -56,14 +60,16 @@ public class GenericTriggerIncludeConverterTest
       Map<String, Object> dataMap = new HashMap<String, Object>();
       dataMap.put("XmlData", xmlMessage);
 
-      ProcessInstance pInstance = sf.getWorkflowService().startProcess("{GenericTriggerConverterTestModel}TestFromXml", dataMap, true);
+      ProcessInstance pInstance = sf.getWorkflowService().startProcess(
+            "{GenericTriggerConverterTestModel}TestFromXml", dataMap, true);
       assertNotNull(pInstance);
       Thread.sleep(1000);
       ProcessInstances pis = sf.getQueryService().getAllProcessInstances(
             ProcessInstanceQuery.findAlive("{GenericTriggerConverterTestModel}FromXml"));
       ProcessInstance pi = pis.get(0);
       sf.getWorkflowService().activateNextActivityInstanceForProcessInstance(pi.getOID());
-      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(pi.getOID(), "Structured");
+      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(
+            pi.getOID(), "Structured");
 
       assertNotNull(response);
       assertTrue(response instanceof Map);
@@ -74,7 +80,8 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(response.get("longField") instanceof Long);
       assertEquals(1222L, response.get("longField"));
       assertTrue(response.get("dateField") instanceof Date);
-      assertEquals("Tue Sep 24 00:00:00 WAT 2002", response.get("dateField").toString());
+      assertEquals("24-09-02", formatDate((Date) response.get("dateField")));
+
    }
 
    @Test
@@ -85,14 +92,16 @@ public class GenericTriggerIncludeConverterTest
       String jsonMessage = "stringField#intField#longField#dateField\nString Field in Csv#33#2222222#Tue May 13 00:00:00 WAT 2014";
       Map<String, Object> dataMap = new HashMap<String, Object>();
       dataMap.put("CsvData", jsonMessage);
-      ProcessInstance pInstance = sf.getWorkflowService().startProcess("{GenericTriggerConverterTestModel}TestFromCSV", dataMap, true);
+      ProcessInstance pInstance = sf.getWorkflowService().startProcess(
+            "{GenericTriggerConverterTestModel}TestFromCSV", dataMap, true);
       assertNotNull(pInstance);
       Thread.sleep(1000);
       ProcessInstances pis = sf.getQueryService().getAllProcessInstances(
             ProcessInstanceQuery.findAlive("{GenericTriggerConverterTestModel}FromCsv"));
       ProcessInstance pi = pis.get(0);
       sf.getWorkflowService().activateNextActivityInstanceForProcessInstance(pi.getOID());
-      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(pi.getOID(), "Structured");
+      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(
+            pi.getOID(), "Structured");
       assertNotNull(response);
       assertTrue(response instanceof Map);
       assertTrue(response.get("intField") instanceof Integer);
@@ -102,7 +111,7 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(response.get("longField") instanceof Long);
       assertEquals(2222222L, response.get("longField"));
       assertTrue(response.get("dateField") instanceof Date);
-      assertEquals("Tue May 13 00:00:00 WAT 2014", response.get("dateField").toString());
+      assertEquals("13-05-14", formatDate((Date) response.get("dateField")));
    }
 
    @Test
@@ -113,14 +122,16 @@ public class GenericTriggerIncludeConverterTest
       String jsonMessage = "{\"intField\":22,\"longField\":11,\"stringField\":\"String Field\",\"dateField\":\"2014-05-12T00:00:00.000\"}";
       Map<String, Object> dataMap = new HashMap<String, Object>();
       dataMap.put("JsonData", jsonMessage);
-      ProcessInstance pInstance = sf.getWorkflowService().startProcess("{GenericTriggerConverterTestModel}TestFromJSON", dataMap, true);
+      ProcessInstance pInstance = sf.getWorkflowService().startProcess(
+            "{GenericTriggerConverterTestModel}TestFromJSON", dataMap, true);
       Thread.sleep(1000);
       assertNotNull(pInstance);
       ProcessInstances pis = sf.getQueryService().getAllProcessInstances(
             ProcessInstanceQuery.findAlive("{GenericTriggerConverterTestModel}FromJson"));
       ProcessInstance pi = pis.get(0);
       sf.getWorkflowService().activateNextActivityInstanceForProcessInstance(pi.getOID());
-      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(pi.getOID(), "Structured");
+      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(
+            pi.getOID(), "Structured");
       assertNotNull(response);
       assertTrue(response instanceof Map);
       assertTrue(response.get("intField") instanceof Integer);
@@ -130,7 +141,7 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(response.get("longField") instanceof Long);
       assertEquals(11L, response.get("longField"));
       assertTrue(response.get("dateField") instanceof Date);
-      assertEquals("Mon May 12 00:00:00 WAT 2014", response.get("dateField").toString());
+      assertEquals("12-05-14", formatDate((Date) response.get("dateField")));
    }
 
    @SuppressWarnings("rawtypes")
@@ -142,14 +153,17 @@ public class GenericTriggerIncludeConverterTest
       String csvListMessage = "stringField#intField#longField#dateField\nFirst Line#12#45454#Tue May 27 00:00:00 WAT 2014\nSecondLine#41#95854#Sun May 18 00:00:00 WAT 2014";
       Map<String, Object> dataMap = new HashMap<String, Object>();
       dataMap.put("CsvListData", csvListMessage);
-      ProcessInstance pInstance = sf.getWorkflowService().startProcess("{GenericTriggerConverterTestModel}TestFromListCSV", dataMap, true);
+      ProcessInstance pInstance = sf.getWorkflowService().startProcess(
+            "{GenericTriggerConverterTestModel}TestFromListCSV", dataMap, true);
       assertNotNull(pInstance);
       Thread.sleep(1000);
       ProcessInstances pis = sf.getQueryService().getAllProcessInstances(
-            ProcessInstanceQuery.findAlive("{GenericTriggerConverterTestModel}FromListCSV"));
+            ProcessInstanceQuery
+                  .findAlive("{GenericTriggerConverterTestModel}FromListCSV"));
       ProcessInstance pi = pis.get(0);
       sf.getWorkflowService().activateNextActivityInstanceForProcessInstance(pi.getOID());
-      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(pi.getOID(), "ListStructured");
+      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(
+            pi.getOID(), "ListStructured");
       assertTrue(response instanceof Map);
       assertNotNull(response);
       List elements = (List) response.get("elmts");
@@ -164,7 +178,7 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(firstElement.get("longField") instanceof Long);
       assertEquals(45454L, firstElement.get("longField"));
       assertTrue(firstElement.get("dateField") instanceof Date);
-      assertEquals("Tue May 27 00:00:00 WAT 2014", firstElement.get("dateField").toString());
+      assertEquals("27-05-14", formatDate((Date) firstElement.get("dateField")));
       assertTrue(secondElement.get("intField") instanceof Integer);
       assertEquals(41, secondElement.get("intField"));
       assertTrue(secondElement.get("stringField") instanceof String);
@@ -172,7 +186,7 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(secondElement.get("longField") instanceof Long);
       assertEquals(95854L, secondElement.get("longField"));
       assertTrue(secondElement.get("dateField") instanceof Date);
-      assertEquals("Sun May 18 00:00:00 WAT 2014", secondElement.get("dateField").toString());
+      assertEquals("18-05-14", formatDate((Date) secondElement.get("dateField")));
    }
 
    @SuppressWarnings("rawtypes")
@@ -183,14 +197,17 @@ public class GenericTriggerIncludeConverterTest
       String jsonListMessage = "[{\"intField\":123,\"longField\":321414,\"stringField\":\"First Element In Json List\",\"dateField\":\"2014-05-01T00:00:00.000\"},{\"intField\":847,\"longField\":215245,\"stringField\":\"SecondElement In Json List\",\"dateField\":\"2014-01-21T00:00:00.000\"}]";
       Map<String, Object> dataMap = new HashMap<String, Object>();
       dataMap.put("JsonListData", jsonListMessage);
-      ProcessInstance pInstance = sf.getWorkflowService().startProcess("{GenericTriggerConverterTestModel}TestFromListJSON", dataMap, true);
+      ProcessInstance pInstance = sf.getWorkflowService().startProcess(
+            "{GenericTriggerConverterTestModel}TestFromListJSON", dataMap, true);
       assertNotNull(pInstance);
       Thread.sleep(1000);
       ProcessInstances pis = sf.getQueryService().getAllProcessInstances(
-            ProcessInstanceQuery.findAlive("{GenericTriggerConverterTestModel}FromListJson"));
+            ProcessInstanceQuery
+                  .findAlive("{GenericTriggerConverterTestModel}FromListJson"));
       ProcessInstance pi = pis.get(0);
       sf.getWorkflowService().activateNextActivityInstanceForProcessInstance(pi.getOID());
-      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(pi.getOID(), "ListStructured");
+      Map< ? , ? > response = (Map< ? , ? >) sf.getWorkflowService().getInDataPath(
+            pi.getOID(), "ListStructured");
       assertTrue(response instanceof Map);
       assertNotNull(response);
       List elements = (List) response.get("elmts");
@@ -206,7 +223,9 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(firstElement.get("longField") instanceof Long);
       assertEquals(321414L, firstElement.get("longField"));
       assertTrue(firstElement.get("dateField") instanceof Date);
-      assertEquals("Thu May 01 00:00:00 WAT 2014", firstElement.get("dateField").toString());
+      // assertEquals("Thu May 01 00:00:00 WAT 2014",
+      // firstElement.get("dateField").toString());
+      assertEquals("01-05-14", formatDate((Date) firstElement.get("dateField")));
       assertTrue(secondElement.get("intField") instanceof Integer);
       assertEquals(847, secondElement.get("intField"));
       assertTrue(secondElement.get("stringField") instanceof String);
@@ -214,8 +233,10 @@ public class GenericTriggerIncludeConverterTest
       assertTrue(secondElement.get("longField") instanceof Long);
       assertEquals(215245L, secondElement.get("longField"));
       assertTrue(secondElement.get("dateField") instanceof Date);
-      assertEquals("Tue Jan 21 00:00:00 WAT 2014", secondElement.get("dateField").toString());
+      // assertEquals("Tue Jan 21 00:00:00 WAT 2014",
+      // secondElement.get("dateField").toString());
+      assertEquals("21-01-14", formatDate((Date) secondElement.get("dateField")));
 
    }
-   
+
 }

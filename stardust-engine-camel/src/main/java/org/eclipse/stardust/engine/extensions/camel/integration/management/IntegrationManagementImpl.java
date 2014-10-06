a91061b62341dbcf62137d867cadb553a9055f14
Melek Zribi <melek.zribi@sungard.com>
2014-10-06 13:29:41 +0000

    CRNT-34149 : Create Management UI
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_1_x/stardust/engine@75067 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-camel/src/main/java/org/eclipse/stardust/engine/extensions/camel/integration/management/IntegrationManagementImpl.java
+++ stardust-engine-camel/src/main/java/org/eclipse/stardust/engine/extensions/camel/integration/management/IntegrationManagementImpl.java
@@ -50,7 +50,64 @@ public class IntegrationManagementImpl implements IntegrationManagement, Applica
 		}
 		return convertObjectToJsonString(routeCamelModelList);
 	}
+	
+	public void startCamelContext(String contextId) {
+		List<CamelContextModel> contexts = initCamelContextModel();
+		CamelContextModel ccm = searchCamelContextModel(contexts,contextId);
+		ccm.start();
+	}
+	
+	public void stopCamelContext(String contextId) {
+		List<CamelContextModel> contexts = initCamelContextModel();
+		CamelContextModel ccm = searchCamelContextModel(contexts,contextId);
+		ccm.stop();
+	}
+	
+	public String getProducerRoutesList(String contextId) {
+		List<CamelContextModel> contexts =initCamelContextModel();
+		CamelContextModel ccm = searchCamelContextModel(contexts,contextId);
+		List<RouteCamelModel> routeCamelModelList = new ArrayList<RouteCamelModel>();
+		if (ccm != null) {
+			for (RouteCamelModel data : ccm.getRoutes()) {
+				if (data.getId().startsWith("Producer")){
+					routeCamelModelList.add(data);
+				}
+			}
+		}
+		return convertObjectToJsonString(routeCamelModelList);
+	}
+	
+	public String getTriggerConsumerRoutesList(String contextId) {
 
+		List<CamelContextModel> contexts =initCamelContextModel();
+		CamelContextModel ccm = searchCamelContextModel(contexts,contextId);
+		List<RouteCamelModel> routeCamelModelList = new ArrayList<RouteCamelModel>();
+		if (ccm != null) {
+			for (RouteCamelModel data : ccm.getRoutes()) {
+				if (data.getId().startsWith("Consumer-")){
+					routeCamelModelList.add(data);
+				}
+			}
+		}
+		return convertObjectToJsonString(routeCamelModelList);	
+		}
+	
+	public String getApplicationConsumerRoutesList(String contextId) {
+		
+		List<CamelContextModel> contexts =initCamelContextModel();
+		CamelContextModel ccm = searchCamelContextModel(contexts,contextId);
+		List<RouteCamelModel> routeCamelModelList = new ArrayList<RouteCamelModel>();
+		if (ccm != null) {
+			for (RouteCamelModel data : ccm.getRoutes()) {
+				if ((data.getId().startsWith("Consumer") && (!data.getId().startsWith("Consumer-")))){
+					routeCamelModelList.add(data);
+				}
+			}
+		}
+		return convertObjectToJsonString(routeCamelModelList);
+
+	}
+	
 	public void startRouteService( String contextId, String routeId) {
 		LOGGER.info("--> Starting route : " + routeId);
 		ModelCamelContext camelcontext=(ModelCamelContext) applicationContext.getBean(contextId);

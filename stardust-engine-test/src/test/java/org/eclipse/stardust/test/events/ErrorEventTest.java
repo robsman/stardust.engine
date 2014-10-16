7f693fef94c3f9ff4371bced8ec62a15289cc3db
Robert Sauer <robert.sauer@sungard.com>
2014-10-16 10:24:36 +0200

    Adding test case simulating the intended Error Event handling approach.
    This will be gradually migrated to the real implementation.

---
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/events/ErrorEventTest.java
@@ -0,0 +1,92 @@
+/**********************************************************************************
+ * Copyright (c) 2012 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ **********************************************************************************/
+package org.eclipse.stardust.test.events;
+
+import static java.util.Collections.singletonMap;
+import static org.eclipse.stardust.test.api.util.TestConstants.MOTU;
+
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import org.eclipse.stardust.engine.api.query.ProcessInstanceQuery;
+import org.eclipse.stardust.engine.api.runtime.ProcessInstance;
+import org.eclipse.stardust.engine.api.runtime.ProcessInstanceState;
+import org.eclipse.stardust.engine.api.runtime.WorkflowService;
+import org.eclipse.stardust.engine.core.runtime.beans.AbortScope;
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSetup;
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSetup.ForkingServiceMode;
+import org.eclipse.stardust.test.api.setup.TestMethodSetup;
+import org.eclipse.stardust.test.api.setup.TestServiceFactory;
+import org.eclipse.stardust.test.api.util.ActivityInstanceStateBarrier;
+import org.eclipse.stardust.test.api.util.ProcessInstanceStateBarrier;
+import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
+
+public class ErrorEventTest
+{
+   /* package-private */static final String ERROR_EVENTS_MODEL_NAME = "ErrorEventTest";
+
+   private static final UsernamePasswordPair ADMIN_USER_PWD_PAIR = new UsernamePasswordPair(MOTU, MOTU);
+
+   private final TestServiceFactory sf = new TestServiceFactory(ADMIN_USER_PWD_PAIR);
+
+   @ClassRule
+   public static final LocalJcrH2TestSetup testClassSetup = new LocalJcrH2TestSetup(ADMIN_USER_PWD_PAIR, ForkingServiceMode.JMS,
+         ERROR_EVENTS_MODEL_NAME);
+
+   private final TestMethodSetup testMethodSetup = new TestMethodSetup(ADMIN_USER_PWD_PAIR, testClassSetup);
+
+   @Rule
+   public final TestRule chain = RuleChain.outerRule(testMethodSetup).around(sf);
+
+   @Before
+   public void init()
+   {}
+
+   @Test
+   public void testThrownErrorEventIsReceivedByCallingActivity() throws InterruptedException, TimeoutException
+   {
+      WorkflowService wfs = sf.getWorkflowService();
+
+      ProcessInstance rootProcess = wfs.startProcess("RootProcess",
+            singletonMap("ProcessKey", Long.toString(System.nanoTime())), true);
+
+      ActivityInstanceStateBarrier aiStateChangeBarrier = ActivityInstanceStateBarrier.instance();
+      ProcessInstanceStateBarrier piStateChangeBarrier = ProcessInstanceStateBarrier.instance();
+
+      // error handling should be activated
+      aiStateChangeBarrier.awaitForId(rootProcess.getOID(), "AwaitError");
+
+      // failing sub-process was started
+      aiStateChangeBarrier.awaitForId(rootProcess.getOID(), "FailingProcess");
+      ProcessInstance failingProcess = sf.getQueryService().findFirstProcessInstance(
+            ProcessInstanceQuery.findForProcess("FailingProcess"));
+
+      // error event was thrown
+      aiStateChangeBarrier.awaitForId(failingProcess.getOID(), "ThrowError");
+      aiStateChangeBarrier.awaitForId(failingProcess.getOID(), "AwaitAbort");
+
+      // await error was received
+      aiStateChangeBarrier.awaitForId(rootProcess.getOID(), "ReceivedError");
+
+      // trigger / await PI completion
+      sf.getWorkflowService().abortProcessInstance(failingProcess.getOID(), AbortScope.SubHierarchy);
+      piStateChangeBarrier.await(failingProcess.getOID(), ProcessInstanceState.Aborted);
+
+      // await root process completion
+      piStateChangeBarrier.await(rootProcess.getOID(), ProcessInstanceState.Completed);
+   }
+}
---
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/events/EventsTestSuite.java
@@ -0,0 +1,35 @@
+/**********************************************************************************
+ * Copyright (c) 2012 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ **********************************************************************************/
+package org.eclipse.stardust.test.events;
+
+import static org.eclipse.stardust.test.api.util.TestConstants.MOTU;
+import static org.eclipse.stardust.test.events.ErrorEventTest.ERROR_EVENTS_MODEL_NAME;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSuiteSetup;
+import org.eclipse.stardust.test.api.setup.LocalJcrH2TestSetup.ForkingServiceMode;
+import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+               ErrorEventTest.class
+             })
+public class EventsTestSuite
+{
+   /* test suite */
+
+   @ClassRule
+   public static final LocalJcrH2TestSuiteSetup testSuiteSetup = new LocalJcrH2TestSuiteSetup(new UsernamePasswordPair(MOTU, MOTU), ForkingServiceMode.JMS, ERROR_EVENTS_MODEL_NAME);
+}

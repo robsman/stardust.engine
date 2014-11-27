7546c66dce8e946fc79aecd478956fd28a49df01
Roland Stamm <roland.stamm@sungard.com>
2014-11-27 13:36:41 +0000

    Jira-ID: CRNT-34780 Added javadoc and copyright header.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_1_x/stardust/engine@75700 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/query/PrefetchConstants.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/query/PrefetchConstants.java
@@ -1,5 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.stardust.engine.api.query;
 
+/**
+ * Constants for query prefetch caches.
+ *
+ * @author Roland.Stamm
+ */
 public class PrefetchConstants
 {
    public static final String HIST_STATE_PERFORMED_ON_BEHALF_OF_USER_CACHE = "Engine.Prefetch.HistoricalState.PerformedOnBehalfOfUser.Cache";

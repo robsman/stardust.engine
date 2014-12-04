3d5a1069b5dd9e833ad5e6e65211dabf543f3580
Stephan Born <stephan.born@sungard.com>
2014-12-04 10:29:38 +0100

    [450449] Upgrade Stardust to 2.1.0 - Adjusted IPP-Stardust version mapping.
    Signed-off-by: Stephan Born <stephan.born@sungard.com>

--- stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
+++ stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 SunGard CSA LLC and others.
+ * Copyright (c) 2011, 2014 SunGard CSA LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,8 +49,9 @@ public class Version implements Comparable<Version>, Serializable
       mapStardust2Ipp.put(Version.createFixedVersion(1, 1, 2), Version.createFixedVersion(7, 2, 0));
       mapStardust2Ipp.put(Version.createFixedVersion(2, 0, 0), Version.createFixedVersion(8, 0, 0));
       mapStardust2Ipp.put(Version.createFixedVersion(2, 0, 1), Version.createFixedVersion(8, 0, 1));
+      mapStardust2Ipp.put(Version.createFixedVersion(2, 1, 0), Version.createFixedVersion(8, 1, 1));
       // map DEV builds to latest IPP release
-      mapStardust2Ipp.put(Version.createFixedVersion(9, 9, 9), Version.createFixedVersion(8, 0, 1));
+      mapStardust2Ipp.put(Version.createFixedVersion(9, 9, 9), Version.createFixedVersion(8, 1, 1));
    }
 
    // some Versions coded in product are fixed and are not allowed to be altered during compare

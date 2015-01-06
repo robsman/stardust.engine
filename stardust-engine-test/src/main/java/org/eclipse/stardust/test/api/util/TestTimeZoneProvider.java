a4111a80883758f1c35239416407bcbb07e85238
Nico Werlein <nicolas.werlein@sungard.com>
2015-01-06 17:59:38 +0100

    added missing license header and java doc
    Signed-off-by: Nico Werlein <nicolas.werlein@sungard.com>

--- stardust-engine-test/src/main/java/org/eclipse/stardust/test/api/util/TestTimeZoneProvider.java
+++ stardust-engine-test/src/main/java/org/eclipse/stardust/test/api/util/TestTimeZoneProvider.java
@@ -1,9 +1,27 @@
+/**********************************************************************************
+ * Copyright (c) 2015 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ **********************************************************************************/
 package org.eclipse.stardust.test.api.util;
 
 import java.util.TimeZone;
 
 import org.eclipse.stardust.common.config.TimeZoneProvider;
 
+/**
+ * <p>
+ * A {@link TimeZoneProvider} implementation allowing for returning a particular time zone regardless
+ * of the test case's actual time zone.
+ * </p>
+ *
+ * @author Nicolas.Werlein
+ */
 public class TestTimeZoneProvider implements TimeZoneProvider
 {
    public static final TimeZone FIXED_TEST_TIME_ZONE = TimeZone.getTimeZone("Europe/Berlin");

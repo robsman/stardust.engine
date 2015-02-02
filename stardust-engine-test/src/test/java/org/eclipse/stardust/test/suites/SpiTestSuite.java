b04b4da8b97914bddea28a71ddf272b46902ba8c
Thomas.Wolfram <thomas.wolfram@sungard.com>
2015-02-02 17:20:31 +0100

    [CRNT-34924]: Support callback method that is invoked after model deployment.
    Added Test to Suite
    
    Bug: CRNT-34924
    Signed-off-by: Thomas.Wolfram <thomas.wolfram@sungard.com>

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/suites/SpiTestSuite.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/suites/SpiTestSuite.java
@@ -6,6 +6,7 @@ import org.eclipse.stardust.test.api.setup.TestClassSetup.ForkingServiceMode;
 import org.eclipse.stardust.test.api.setup.TestSuiteSetup;
 import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
 import org.eclipse.stardust.test.spi.FastCachingSequenceGeneratorTest;
+import org.eclipse.stardust.test.spi.PartitionMonitorTest;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -13,17 +14,19 @@ import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * <p>
- * This test suite bundles tests for <i>SPI</i> functionality,
- * which allows for integrating custom behaviour via the SPI mechanism
- * (refer to the Stardust documentation for details about available SPIs).
+ * This test suite bundles tests for <i>SPI</i> functionality, which allows for
+ * integrating custom behaviour via the SPI mechanism (refer to the Stardust documentation
+ * for details about available SPIs).
  * </p>
  *
  * @author Nicolas.Werlein
  */
 @RunWith(Suite.class)
-@SuiteClasses({ FastCachingSequenceGeneratorTest.class })
+@SuiteClasses({FastCachingSequenceGeneratorTest.class,
+               PartitionMonitorTest.class})
 public class SpiTestSuite
 {
    @ClassRule
-   public static final TestSuiteSetup testSuiteSetup = new TestSuiteSetup(new UsernamePasswordPair(MOTU, MOTU), ForkingServiceMode.NATIVE_THREADING);
+   public static final TestSuiteSetup testSuiteSetup = new TestSuiteSetup(
+         new UsernamePasswordPair(MOTU, MOTU), ForkingServiceMode.NATIVE_THREADING, "BasicWorkflowModel");
 }

1854c636331bf9b582a924844f1437dd459411ba
Roland Stamm <roland.stamm@sungard.com>
2015-05-28 15:35:13 +0200

    [CRNT-36812]: Time Square Kernel Changes
     Added an all blocked timeOffCalendar deployed on test case setup.
     
    Bug: CRNT-36812
    Signed-off-by: Roland Stamm <roland.stamm@sungard.com>

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/benchmarks/BenchmarksTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/benchmarks/BenchmarksTest.java
@@ -2,36 +2,30 @@ package org.eclipse.stardust.test.benchmarks;
 
 import static org.eclipse.stardust.test.api.util.TestConstants.MOTU;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Date;
 
-import org.eclipse.stardust.engine.api.query.ActivityInstanceQuery;
-import org.eclipse.stardust.engine.api.query.ActivityInstances;
-import org.eclipse.stardust.engine.api.runtime.ActivityInstance;
-import org.eclipse.stardust.engine.api.runtime.AdministrationService;
-import org.eclipse.stardust.engine.api.runtime.DaemonExecutionState;
-import org.eclipse.stardust.engine.api.runtime.ProcessInstance;
-import org.eclipse.stardust.engine.api.runtime.RuntimeArtifact;
-import org.eclipse.stardust.engine.api.runtime.StartOptions;
-import org.eclipse.stardust.engine.core.spi.artifact.ArtifactManagerFactory;
-import org.eclipse.stardust.engine.core.spi.artifact.impl.BenchmarkDefinitionArtifactType;
-import org.eclipse.stardust.test.api.setup.TestClassSetup;
-import org.eclipse.stardust.test.api.setup.TestClassSetup.ForkingServiceMode;
-import org.eclipse.stardust.test.api.setup.TestMethodSetup;
-import org.eclipse.stardust.test.api.setup.TestServiceFactory;
-import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
+import org.eclipse.stardust.engine.api.query.ActivityInstanceQuery;
+import org.eclipse.stardust.engine.api.query.ActivityInstances;
+import org.eclipse.stardust.engine.api.runtime.*;
+import org.eclipse.stardust.engine.core.spi.artifact.impl.BenchmarkDefinitionArtifactType;
+import org.eclipse.stardust.test.api.setup.*;
+import org.eclipse.stardust.test.api.setup.TestClassSetup.ForkingServiceMode;
+import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
+
 /**
  * Class to test benchmark functionality
- * 
+ *
  * @author Thomas.Wolfram
  *
  */
@@ -64,32 +58,78 @@ public class BenchmarksTest
    private static final String BENCHMARK_PROCESS = "BenchmarkedProcess";
 
    private static final String BENCHMARK_PROCESS_W_SUB = "BenchmarkedParentProcess";
-   
+
    private static final String BENCHMARK_ARTIFACT_TYPE_ID = BenchmarkDefinitionArtifactType.TYPE_ID;
-   
+
    private static final String ARTIFACT_ID = "bench1.benchmark";
-   
-   private static final String ARTIFACT_NAME = "Benchmark One";   
-   
-   private static final String ARTIFACT_CONTENT = "true;";   
+
+   private static final String ARTIFACT_NAME = "Benchmark One";
+
+   private static final String ARTIFACT_CONTENT = "true;";
 
    @Before
    public void setup()
    {
+      deployCalendar("timeOffCalendar-d76edddf-361f-4423-8f70-de8d72b1d277.json");
       serviceFactory.getAdministrationService().deployRuntimeArtifact(getRuntimeArtifact(ARTIFACT_ID));
 
       startOptions_withBenchmark = new StartOptions(null, true, BENCHMARK_TEST_REF);
       startOptions_withoutBenchmark = new StartOptions(null, true);
    }
 
+   private void deployCalendar(String calendarName)
+   {
+      DocumentManagementService dms = serviceFactory.getDocumentManagementService();
+
+      final String parentFolder = "/business-calendars/timeOffCalendar";
+
+      if(dms.getFolder(parentFolder)!= null)
+      {
+         return;
+      }
+
+      final String calendarFilePath = "binaryFiles/"+ calendarName;
+      final InputStream is = RtEnvHome.class.getClassLoader().getResourceAsStream(calendarFilePath);
+      BufferedReader br = new BufferedReader(new InputStreamReader(is));
+      StringBuilder sb = new StringBuilder();
+      String read;
+      try
+      {
+         read = br.readLine();
+         while (read != null)
+         {
+            sb.append(read);
+            read= br.readLine();
+         }
+      }
+      catch (IOException e)
+      {
+        Assert.fail(e.getMessage());
+      }
+
+      DocumentInfo document = DmsUtils.createDocumentInfo(calendarName);
+      byte[] content = sb.toString().getBytes();
+
+
+      DmsUtils.ensureFolderHierarchyExists(parentFolder, dms);
+
+      Document calDoc = dms.createDocument(parentFolder, document, content, null);
+
+      byte[] checkContent = dms
+            .retrieveDocumentContent(calDoc.getId());
+      Assert.assertNotNull(checkContent);
+      Assert.assertNotEquals(0, checkContent.length);
+   }
+
    @Test
    public void startProcessInstanceWithBenchmarkTest()
    {
-      
+
       ProcessInstance pi = serviceFactory.getWorkflowService().startProcess(
             BENCHMARK_PROCESS, startOptions_withBenchmark);
 
-      assertEquals(BENCHMARK_TEST_REF, pi.getBenchmark());
+      assertTrue(0 < pi.getBenchmark());
+      assertTrue(0 == pi.getBenchmarkValue());
 
    }
 
@@ -103,7 +143,7 @@ public class BenchmarksTest
             .findFirstActivityInstance(ActivityInstanceQuery.findAlive());
 
       System.out.println("#### Benchmark Value " + instance.getBenchmarkValue());
-      
+
    }
 
    @Test
@@ -156,11 +196,11 @@ public class BenchmarksTest
 
    }
 
-   
+
    private RuntimeArtifact getRuntimeArtifact(String artifactId)
    {
       return new RuntimeArtifact(BENCHMARK_ARTIFACT_TYPE_ID, artifactId, ARTIFACT_NAME,
             ARTIFACT_CONTENT.getBytes(), new Date(1));
    }
-   
+
 }

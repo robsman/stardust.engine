96892cbb45eb325f3994e6cf5d9fe815d194f118
Stephan Born <stephan.born@sungard.com>
2015-12-08 18:22:28 +0100

    [473469]: Adjusted IPP-Stardust version mapping: 3.1.0 -> 8.2.2
    Bug: 473469

--- stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
+++ stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
@@ -53,6 +53,7 @@ public class Version implements Comparable<Version>, Serializable
       mapStardust2Ipp.put(Version.createFixedVersion(2, 1, 1), Version.createFixedVersion(8, 1, 1));
       mapStardust2Ipp.put(Version.createFixedVersion(3, 0, 0), Version.createFixedVersion(8, 2, 0));
       mapStardust2Ipp.put(Version.createFixedVersion(3, 0, 1), Version.createFixedVersion(8, 2, 2));
+      mapStardust2Ipp.put(Version.createFixedVersion(3, 1, 0), Version.createFixedVersion(8, 2, 2));
 
       // map DEV builds to latest IPP release
       mapStardust2Ipp.put(Version.createFixedVersion(9, 9, 9), Version.createFixedVersion(8, 2, 2));

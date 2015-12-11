d97bf5391d75d0d3fcf83277ea69ae89d8a60ed9
Stephan Born <stephan.born@sungard.com>
2015-12-11 10:34:03 +0100

    [474122]: Added ersion mapping from SD 4.0.0 to IPP 9.2.0
    Bug: 474122

--- stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
+++ stardust-engine-base/src/main/java/org/eclipse/stardust/common/config/Version.java
@@ -54,6 +54,7 @@ public class Version implements Comparable<Version>, Serializable
       mapStardust2Ipp.put(Version.createFixedVersion(2, 1, 1), Version.createFixedVersion(8, 1, 1));
       mapStardust2Ipp.put(Version.createFixedVersion(3, 0, 0), Version.createFixedVersion(8, 2, 0));
       mapStardust2Ipp.put(Version.createFixedVersion(3, 0, 1), Version.createFixedVersion(8, 2, 2));
+      mapStardust2Ipp.put(Version.createFixedVersion(4, 0, 0), Version.createFixedVersion(9, 2, 0));
 
       // map DEV builds to latest IPP release
       mapStardust2Ipp.put(Version.createFixedVersion(9, 9, 9), Version.createFixedVersion(9, 2, 0));

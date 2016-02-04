a7877beac837cfacfe1d5e1d67554777d29786d2
Florin Herinean <florin.herinean@sungard.com>
2016-02-04 16:28:32 +0100

    Jira-ID: CRNT-39363 Composite descriptors
    Added test cases.

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/data/PrimitiveDataInOutDataPathsTest.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/data/PrimitiveDataInOutDataPathsTest.java
@@ -18,19 +18,19 @@ import static org.junit.Assert.assertThat;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 
 import org.eclipse.stardust.common.error.ObjectNotFoundException;
+import org.eclipse.stardust.engine.api.query.DescriptorPolicy;
+import org.eclipse.stardust.engine.api.query.ProcessInstanceQuery;
 import org.eclipse.stardust.engine.api.runtime.ProcessInstance;
 import org.eclipse.stardust.test.api.setup.TestClassSetup;
 import org.eclipse.stardust.test.api.setup.TestClassSetup.ForkingServiceMode;
 import org.eclipse.stardust.test.api.setup.TestMethodSetup;
 import org.eclipse.stardust.test.api.setup.TestServiceFactory;
 import org.eclipse.stardust.test.api.util.UsernamePasswordPair;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
@@ -39,32 +39,32 @@ import org.junit.rules.TestRule;
  * Tests whether setting and retrieving primitive process data
  * via in and out data paths works correctly.
  * </p>
- * 
+ *
  * @author Nicolas.Werlein
  * @version $Revision$
  */
 public class PrimitiveDataInOutDataPathsTest
 {
    private static final UsernamePasswordPair ADMIN_USER_PWD_PAIR = new UsernamePasswordPair(MOTU, MOTU);
-   
+
    private final TestMethodSetup testMethodSetup = new TestMethodSetup(ADMIN_USER_PWD_PAIR, testClassSetup);
    private final TestServiceFactory sf = new TestServiceFactory(ADMIN_USER_PWD_PAIR);
-   
+
    @ClassRule
    public static final TestClassSetup testClassSetup = new TestClassSetup(ADMIN_USER_PWD_PAIR, ForkingServiceMode.NATIVE_THREADING, MODEL_NAME);
-   
+
    @Rule
    public final TestRule chain = RuleChain.outerRule(testMethodSetup)
                                           .around(sf);
-   
+
    private long piOid;
-   
+
    @Before
    public void setUp()
    {
       piOid = startProcess();
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>Calendar</i> process data
@@ -76,7 +76,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Calendar.getInstance(), MY_CALENDAR_IN_DATA_PATH, MY_CALENDAR_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>String</i> process data
@@ -88,7 +88,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor("This is a test.", MY_STRING_IN_DATA_PATH, MY_STRING_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>Timestamp</i> process data
@@ -100,7 +100,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(new Date(System.currentTimeMillis()), MY_TIMESTAMP_IN_DATA_PATH, MY_TIMESTAMP_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>boolean</i> process data
@@ -112,7 +112,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Boolean.TRUE, MY_BOOLEAN_IN_DATA_PATH, MY_BOOLEAN_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>byte</i> process data
@@ -125,7 +125,7 @@ public class PrimitiveDataInOutDataPathsTest
       final byte b = 8;
       testFor(Byte.valueOf(b), MY_BYTE_IN_DATA_PATH, MY_BYTE_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>char</i> process data
@@ -137,7 +137,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Character.valueOf('x'), MY_CHAR_IN_DATA_PATH, MY_CHAR_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>double</i> process data
@@ -149,7 +149,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Double.valueOf(81.18), MY_DOUBLE_IN_DATA_PATH, MY_DOUBLE_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>float</i> process data
@@ -161,7 +161,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Float.valueOf(18.81F), MY_FLOAT_IN_DATA_PATH, MY_FLOAT_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>int</i> process data
@@ -173,7 +173,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(81, MY_INT_IN_DATA_PATH, MY_INT_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>long</i> process data
@@ -185,7 +185,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       testFor(Long.valueOf(818L), MY_LONG_IN_DATA_PATH, MY_LONG_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether setting and retrieving a <i>short</i> process data
@@ -198,7 +198,7 @@ public class PrimitiveDataInOutDataPathsTest
       final short s = 18;
       testFor(Short.valueOf(s), MY_SHORT_IN_DATA_PATH, MY_SHORT_OUT_DATA_PATH);
    }
-   
+
    /**
     * <p>
     * Tests whether the correct exception is thrown when the in data path does not exist.
@@ -209,7 +209,7 @@ public class PrimitiveDataInOutDataPathsTest
    {
       sf.getWorkflowService().getInDataPath(piOid, "N/A");
    }
-   
+
    /**
     * <p>
     * Tests whether the correct exception is thrown when the out data path does not exist.
@@ -220,19 +220,79 @@ public class PrimitiveDataInOutDataPathsTest
    {
       sf.getWorkflowService().setOutDataPath(piOid, "N/A", "<Value>");
    }
-   
+
+   /**
+    * <p>
+    * Tests whether the correct exception is thrown when the in data path does not exist.
+    * </p>
+    */
+   @Test
+   public void testCompositeInDataPath()
+   {
+      final String retrievedValue = (String) sf.getWorkflowService().getInDataPath(piOid, "FullName");
+
+      assertThat(retrievedValue, equalTo("Frank Underwood"));
+   }
+
+   @Test
+   public void testSimpleCompositeDescriptor()
+   {
+      testDescriptor("Frank Underwood", "FullName");
+   }
+
+   @Test
+   public void testLinkDescriptor()
+   {
+      testDescriptor("http://fisglobal.com?id=666", "Invoice");
+   }
+
+   @Test
+   public void testTwoLevelCompositeDescriptor()
+   {
+      testDescriptor("Name: Frank Underwood, URL: http://fisglobal.com?id=666", "Composite2Levels");
+   }
+
+   @Test
+   public void testCircularCompositeDescriptor()
+   {
+      testDescriptor("*+-#", "CircularEnd");
+   }
+
+   @Test
+   public void testMultiLevelCircularCompositeDescriptor()
+   {
+      testDescriptor("Composition [Name: Frank Underwood, URL: http://fisglobal.com?id=666, +*#-]", "Composite3Levels");
+   }
+
    private long startProcess()
    {
-      final ProcessInstance pi = sf.getWorkflowService().startProcess(PROCESS_ID_1, null, true);
+      final ProcessInstance pi = sf.getWorkflowService().startProcess(PROCESS_ID_1,
+            Collections.singletonMap("invoiceId", "666"), true);
       return pi.getOID();
    }
-   
+
+   private <T extends Serializable> void testDescriptor(final T value, final String descriptorId)
+   {
+      ProcessInstanceQuery query = ProcessInstanceQuery.findAll();
+      query.where(ProcessInstanceQuery.OID.isEqual(piOid));
+      query.setPolicy(DescriptorPolicy.WITH_DESCRIPTORS);
+      ProcessInstance pi = sf.getQueryService().findFirstProcessInstance(query);
+
+      @SuppressWarnings("unchecked")
+      final T descriptorValue = (T) pi.getDescriptorValue(descriptorId);
+      assertThat(descriptorValue, equalTo(value));
+
+      @SuppressWarnings("unchecked")
+      final T dataPathValue = (T) sf.getWorkflowService().getInDataPath(piOid, descriptorId);
+      assertThat(dataPathValue, equalTo(value));
+   }
+
    private <T extends Serializable> void testFor(final T value, final String inDataPath, final String outDataPath)
    {
       sf.getWorkflowService().setOutDataPath(piOid, outDataPath, value);
       @SuppressWarnings("unchecked")
       final T retrievedValue = (T) sf.getWorkflowService().getInDataPath(piOid, inDataPath);
-      
+
       assertThat(retrievedValue, notNullValue());
       assertThat(retrievedValue, equalTo(value));
    }

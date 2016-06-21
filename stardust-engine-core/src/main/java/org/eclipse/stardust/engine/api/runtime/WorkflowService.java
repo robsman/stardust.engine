8494e99730da643996c5cf311709ff632c6b17f4
Roland Stamm <roland.stamm@sungard.com>
2016-06-21 15:18:12 +0200

    [CRNT-40802] If Delegation grant is disabled, user cannot save and suspend an activity
    Changed declarative security for suspend methods that allow to choose
    participant or user via parameter to "performActivity" with implied
    "delegateToOther, delegateToDepartment".
    
    Bug: CRNT-40802
    Signed-off-by: Roland Stamm <roland.stamm@sungard.com>

--- stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/runtime/WorkflowService.java
+++ stardust-engine-core/src/main/java/org/eclipse/stardust/engine/api/runtime/WorkflowService.java
@@ -524,10 +524,12 @@ public interface WorkflowService extends Service
     * @see #suspendToUser(long, long, String, Map)
     */
    @ExecutionPermission(
-         id=ExecutionPermission.Id.delegateToOther,
+         id=ExecutionPermission.Id.performActivity,
          scope=ExecutionPermission.Scope.activity,
-         defaults={ExecutionPermission.Default.ALL},
-         implied={ExecutionPermission.Id.delegateToDepartment})
+         defaults={ExecutionPermission.Default.OWNER},
+         changeable=false,
+         administratorOverride=false,
+         implied={ExecutionPermission.Id.delegateToOther, ExecutionPermission.Id.delegateToDepartment})
    ActivityInstance suspendToUser(long activityInstanceOID, long userOID)
          throws ObjectNotFoundException, ConcurrencyException, AccessForbiddenException, InvalidArgumentException;
 
@@ -559,10 +561,12 @@ public interface WorkflowService extends Service
     * @see #suspendToUser(long, long)
     */
    @ExecutionPermission(
-         id=ExecutionPermission.Id.delegateToOther,
+         id=ExecutionPermission.Id.performActivity,
          scope=ExecutionPermission.Scope.activity,
-         defaults={ExecutionPermission.Default.ALL},
-         implied={ExecutionPermission.Id.delegateToDepartment})
+         defaults={ExecutionPermission.Default.OWNER},
+         changeable=false,
+         administratorOverride=false,
+         implied={ExecutionPermission.Id.delegateToOther, ExecutionPermission.Id.delegateToDepartment})
    ActivityInstance suspendToUser(long activityInstanceOID, long userOID, String context,
          Map<String, ? > outData) throws ObjectNotFoundException, ConcurrencyException,
          AccessForbiddenException, InvalidArgumentException;
@@ -591,10 +595,12 @@ public interface WorkflowService extends Service
     * @see #suspendToParticipant(long, String, String, Map)
     */
    @ExecutionPermission(
-         id=ExecutionPermission.Id.delegateToOther,
+         id=ExecutionPermission.Id.performActivity,
          scope=ExecutionPermission.Scope.activity,
-         defaults={ExecutionPermission.Default.ALL},
-         implied={ExecutionPermission.Id.delegateToDepartment})
+         defaults={ExecutionPermission.Default.OWNER},
+         changeable=false,
+         administratorOverride=false,
+         implied={ExecutionPermission.Id.delegateToOther, ExecutionPermission.Id.delegateToDepartment})
    ActivityInstance suspendToParticipant(long activityInstanceOID, String participant)
          throws ObjectNotFoundException, ConcurrencyException, AccessForbiddenException;
 
@@ -623,10 +629,12 @@ public interface WorkflowService extends Service
     * @see #suspendToParticipant(long, String)
     */
    @ExecutionPermission(
-         id=ExecutionPermission.Id.delegateToOther,
+         id=ExecutionPermission.Id.performActivity,
          scope=ExecutionPermission.Scope.activity,
-         defaults={ExecutionPermission.Default.ALL},
-         implied={ExecutionPermission.Id.delegateToDepartment})
+         defaults={ExecutionPermission.Default.OWNER},
+         changeable=false,
+         administratorOverride=false,
+         implied={ExecutionPermission.Id.delegateToOther, ExecutionPermission.Id.delegateToDepartment})
    ActivityInstance suspendToParticipant(long activityInstanceOID, String participant,
          String context, Map<String, ? > outData) throws ObjectNotFoundException,
          ConcurrencyException, AccessForbiddenException, InvalidArgumentException;
@@ -656,10 +664,12 @@ public interface WorkflowService extends Service
     *         and the passed participant is a user who worked on the previous workflow instance
     */
    @ExecutionPermission(
-         id=ExecutionPermission.Id.delegateToOther,
+         id=ExecutionPermission.Id.performActivity,
          scope=ExecutionPermission.Scope.activity,
-         defaults={ExecutionPermission.Default.ALL},
-         implied={ExecutionPermission.Id.delegateToDepartment})
+         defaults={ExecutionPermission.Default.OWNER},
+         changeable=false,
+         administratorOverride=false,
+         implied={ExecutionPermission.Id.delegateToOther, ExecutionPermission.Id.delegateToDepartment})
    ActivityInstance suspendToParticipant(long activityInstanceOID, ParticipantInfo participant,
          ContextData outData) throws ObjectNotFoundException, AccessForbiddenException, InvalidArgumentException;
 

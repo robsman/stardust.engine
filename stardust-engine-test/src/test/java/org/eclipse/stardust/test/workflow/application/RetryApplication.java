feab7f6d3dde000a0198cc70c9ceab18a0bb6a8b
Barry Grotjahn <barry.grotjahn@sungard.com>
2016-08-19 13:02:59 +0200

    CRNT-39941 adding missing header

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/workflow/application/RetryApplication.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/workflow/application/RetryApplication.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+* Copyright (c) 2016 SunGard CSA LLC and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Barry.Grotjahn (SunGard CSA LLC) - initial API and implementation and/or initial documentation
+*******************************************************************************/
+
 package org.eclipse.stardust.test.workflow.application;
 
 import java.lang.reflect.InvocationTargetException;

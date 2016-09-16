ea8d361eda0f7e62e90ad6431ccbeef03c8d2177
Barry Grotjahn <barry.grotjahn@sungard.com>
2016-09-16 08:44:36 +0200

    CRNT-41516 adding test to suite

--- stardust-engine-test/src/test/java/org/eclipse/stardust/test/suites/BOTestSuite.java
+++ stardust-engine-test/src/test/java/org/eclipse/stardust/test/suites/BOTestSuite.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import org.eclipse.stardust.test.bo.BusinessDateTest;
 import org.eclipse.stardust.test.bo.BusinessObjectsModelExtenderTest;
 import org.eclipse.stardust.test.businessobject.BusinessObjectDepartmentTest;
 import org.eclipse.stardust.test.businessobject.BusinessObjectsTest;
@@ -21,7 +22,7 @@ import org.eclipse.stardust.test.businessobject.CreateBusinessObjectTest;
 
 /**
  * <p>
- * This test suite bundles BO related tests.
+ * This test suite bundles BO BD related tests.
  * </p>
  *
  * @author Barry.Grotjahn
@@ -31,7 +32,8 @@ import org.eclipse.stardust.test.businessobject.CreateBusinessObjectTest;
    BusinessObjectsModelExtenderTest.class,
    BusinessObjectsTest.class,
    CreateBusinessObjectTest.class,
-   BusinessObjectDepartmentTest.class
+   BusinessObjectDepartmentTest.class,
+   BusinessDateTest.class
              })
 public class BOTestSuite
 {
